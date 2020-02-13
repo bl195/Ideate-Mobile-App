eeea76d4bd6af0047ecdfc257aee33037a4358e6
Brian Li <bl195@duke.edu>
2020-02-13 16:29:36 -0500

    start

---
+++ Ideate/Ideate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ideate
+//
+//  Created by Brian Li on 2/13/20.
+//  Copyright © 2020 HackDuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
