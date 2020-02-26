7da1a062f01b556156a1c02e885eaf7a5e850537
Jessica Su <js803@duke.edu>
2020-02-25 19:36:43 -0500

    adding controller classes for view

---
+++ Ideate/Ideate/FAQViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FAQViewController.swift
+//  Ideate
+//
+//  Created by Jessica Su on 2/25/20.
+//  Copyright © 2020 HackDuke. All rights reserved.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
